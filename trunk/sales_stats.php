3f930037da948db00908cfd5affa4e006b8d7f49
superartstudio <superartstudio@fb9a91f4-b0bc-11dd-870a-3d4ce4bc4118>
2008-11-25 11:45:15 +0000

    added sales stats script

---
+++ trunk/sales_stats.php
@@ -0,0 +1,121 @@
+#!/usr/bin/php
+<?php
+
+/*
+ * Important notice:
+ * 		This script is based on Erica's perl script. Visit the URL below to check the original blog.
+ * 		http://blogs.oreilly.com/iphone/2008/08/scraping-appstore-reviews.html
+ *
+ * 		Leo, www.superarts.org
+ */
+function get_item($s, $index)
+{
+	$head = 0;
+	for ($i = 0; $i < $index; $i++)
+	{
+		$head = strpos($s, "\t", $head + 1);
+	}
+	$tail = strpos($s, "\t", $head + 1);
+	$ret = substr($s, $head + 1, $tail - $head - 1);
+	//	echo "$head - $tail = '$ret'\n";
+
+	return $ret;
+}
+
+function get_name($s)
+{
+	$ret = $s;
+	$ret = str_replace(' ', '_', $ret);
+	$ret = str_replace("'", '_', $ret);
+	$ret = str_replace('"', '_', $ret);
+	$ret = str_replace('+', '_', $ret);
+	$ret = str_replace('.', '_', $ret);
+	$ret = str_replace('~', '_', $ret);
+	$ret = str_replace('`', '_', $ret);
+	$ret = str_replace('/', '_', $ret);
+	$ret = str_replace('\\', '_', $ret);
+
+	//	echo "$s\n$ret\n";
+	return $ret;
+}
+
+function get_content($s)
+{
+	$ret = $s;
+	$ret = str_replace("*  | \n\n", '', $ret);
+	$ret = str_replace("*  | \n", '', $ret);
+	$ret = str_replace("&gt;", '>', $ret);
+
+	//	echo "$s\n$ret\n";
+	return $ret;
+}
+
+$filename = "trend.txt";
+$a = file($filename);
+//	print_r($a);
+
+$array_sku = array();
+$array_name = array();
+$array_id = array();
+
+for ($i = 1; $i < count($a); $i++)
+{
+	$sku = get_item($a[$i], 2);
+	$name = get_item($a[$i], 6);
+	$type = get_item($a[$i], 7);
+	$count = get_item($a[$i], 8);
+	$commission = get_item($a[$i], 9);
+	$begin = get_item($a[$i], 10);
+	$end = get_item($a[$i], 11);
+	$currency = get_item($a[$i], 12);
+	$country = get_item($a[$i], 13);
+	$id = get_item($a[$i], 19);
+	$price = get_item($a[$i], 20);
+
+	$array_sku[] = $sku;
+	$array_name[] = $name;
+	$array_id[] = $id;
+
+	//echo "$i:\t$sku\t$name\n";
+	echo "$type\t$count\t$commission\t$begin\t$end\t$currency\t$country\t$id\t$price\n";
+}
+
+$array_sku = array_unique($array_sku);
+$array_name = array_unique($array_name);
+$array_id = array_unique($array_id);
+
+//	print_r($array_sku);
+
+foreach ($array_sku as $i => $s)
+{
+	//	echo "$i\t$s\n";
+	$sku = $array_sku[$i];
+	$name = $array_name[$i];
+	$id = $array_id[$i];
+
+	//	echo "Retrieving $name,	$sku,	$id...\n";
+
+	$sku = get_name($sku);
+	$name = get_name($name);
+
+	//	exec("./get_appstore_review.perl $id > ./output/$sku-$name.txt");
+}
+
+/*
+exec("ls ./output/*", $r);
+//	print_r($r);
+$fp = fopen("all_reviews.txt", "wb");
+echo "Generating all_reviews.txt...\n";
+
+foreach ($r as $name)
+{
+	$s = file_get_contents($name);
+	$s = get_content($s);
+	fwrite($fp, "---- start of '$name' ----\n");
+	fwrite($fp, $s);
+	fwrite($fp, "---- end of '$name' ----\n\n");
+}
+fclose($fp);
+ */
+
+?>
